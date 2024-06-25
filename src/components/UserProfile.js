b5ed8c8ca85b099d7edec572889f41de8958f845
llckumadevelop <kumauser01@kuma.ink>
2024-06-25 21:37:57 +0700

    update code

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import "../components/UserProfile.css";
 
 class UserProfile extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h2>componetns</h2>
+      <div className="m-5">
+        <h1 className="text-center">Use Profile</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <div className="group">
+                <input type="text" required />
+                <span className="hightlight" />
+                <span className="bar" />
+                <label>Name</label>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="group">
+                <input type="text" required />
+                <span className="hightlight" />
+                <span className="bar" />
+                <label>Name</label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
