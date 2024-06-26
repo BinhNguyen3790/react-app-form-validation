6a4956f82dade17eed6e091faba353f9a3e8bef6
llckumadevelop <kumauser01@kuma.ink>
2024-06-26 08:46:35 +0700

    update code

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -5,25 +5,62 @@ class UserProfile extends Component {
   state = {};
   render() {
     return (
-      <div className="m-5">
-        <h1 className="text-center">Use Profile</h1>
+      <div className="p-5 bg-light vh-100">
+        <h1 className="text-center mb-5">Use Profile</h1>
         <div className="container">
           <div className="row">
             <div className="col-6">
               <div className="group">
-                <input type="text" required />
+                <input type="text" name="firstname" required />
                 <span className="hightlight" />
                 <span className="bar" />
-                <label>Name</label>
+                <label>First Name</label>
               </div>
             </div>
             <div className="col-6">
               <div className="group">
-                <input type="text" required />
+                <input type="text" name="lastname" required />
                 <span className="hightlight" />
                 <span className="bar" />
-                <label>Name</label>
+                <label>Last Name</label>
               </div>
+            </div>
+            <div className="col-12">
+              <div className="group">
+                <input type="text" name="username" required />
+                <span className="hightlight" />
+                <span className="bar" />
+                <label>User Name</label>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="group">
+                <input type="email" name="email" required />
+                <span className="hightlight" />
+                <span className="bar" />
+                <label>Email</label>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="group">
+                <input type="text" name="password" required />
+                <span className="hightlight" />
+                <span className="bar" />
+                <label>Password</label>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="group">
+                <input type="text" name="passwordConfirm" required />
+                <span className="hightlight" />
+                <span className="bar" />
+                <label>Password Confirm</label>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <button className="btn btn-lg text-white bg-dark w-100">Submit</button>
             </div>
           </div>
         </div>
